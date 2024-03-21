65b7407613c7e1be6ae6460ab549d7c842855c12
donald <donaldford05091997@gmail.com>
2024-03-21 16:17:21 -0700

    edited some of the exploits to use the data.json file added netcat, adb and fastboot installation in set up updated the handler

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-sudo apt-get install python3-prompt-toolkit
-sudo apt-get install python3-pyfiglet
+sudo apt-get install python3-prompt-toolkit -y
+sudo apt-get install python3-pyfiglet -y
+sudo apt-get install netcat-traditional adb fastboot pip -y
+pip install --break-system-packages pure-python-adb
