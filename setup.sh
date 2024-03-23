d19086275dced92ab591eac178ff90369cfef710
donald <donaldford05091997@gmail.com>
2024-03-23 11:07:05 -0700

    started recon-ng support

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ sudo apt-get install netcat-traditional adb fastboot pip -y
 pip install --break-system-packages pure-python-adb pwn pybluez
 bash <( curl -sSL https://raw.githubusercontent.com/sundowndev/phoneinfoga/master/support/scripts/install )
 sudo mv ./phoneinfoga /usr/local/bin/phoneinfoga
+cd .data && git clone https://github.com/lanmaster53/recon-ng.git
+cd recon-ng && pip install --break-system-packages -r REQUIREMENTS
